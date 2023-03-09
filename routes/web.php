29d17d50f4754ba677c10e2f4621e7186d3d40a6
Larry Bluestoneapps <larry@bluestoneapps.com>
2023-03-09 11:04:46 -0500

    Implemented chat functionality

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
 
 Route::view('/users', 'users.showAll')->name('users.all');
 
+
+Route::get('/chat', 'App\Http\Controllers\ChatController@showChat')->name('chat.show');
+Route::post('/chat/message', 'App\Http\Controllers\ChatController@messageReceived')->name('chat.message');
+Route::post('/chat/greet/{user}', 'App\Http\Controllers\ChatController@greetReceived')->name('chat.greet');
